24ee9da234fba21ee882237374c9b0c9ed4dff7e
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2018-03-07 23:02:27 -0400

    Contador, creacion de contador, eventos incrementar y disminuir creado de manera simple

---
+++ vuex/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container">
+    <h1>Counter</h1>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+
+import Counter from './components/Counter';
+
+export default {
+  components: {
+    Counter
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ vuex/src/components/Decrement.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="decrement" class="btn btn-danger">Reducir</button>
+</template>
+<script>
+export default {
+  
+    methods: {
+        decrement() {
+            this.$emit('decrement');
+        }
+    }
+
+}
+</script>
